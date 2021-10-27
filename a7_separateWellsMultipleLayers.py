608aa3a9e030df8349cb56e651b02e488279350d
moinabyssinia <michaelg.tadesse@gmail.com>
2021-10-27 15:40:00 -0400

    traversing wells separation

--- a7_separateWellsMultipleLayers.py
+++ a7_separateWellsMultipleLayers.py
@@ -11,6 +11,7 @@ saves them separately
 """
 import os 
 import pandas as pd
+import datetime as dt
 
 dirHome = "C:\\Users\\mtadesse\\Hazen and Sawyer\\MIKE_Modeling_Group - Documents\\"\
         "ECFTX\\extractedWellData\\011-allECFTXPermits-county-useclass\\analysis\\rawFile"
@@ -18,34 +19,51 @@ dirOut = "C:\\Users\\mtadesse\\Hazen and Sawyer\\MIKE_Modeling_Group - Documents
         "ECFTX\\extractedWellData\\011-allECFTXPermits-county-useclass\\analysis\\wellMultipleLayers"
 
 
-# read data
-# replace "AVE" with "DEC"
-# concatenate mon and year
-# concatenate row-columns-name
-# resume with the rest of the analysis
-
+os.chdir(dirHome)
 
 
 # read raw data
 dat = pd.read_csv("ecftx_clipped_v4.txt")
-dat.reset_index(inplace = True)
-dat.drop(['Unnamed: 0', 'Unnamed: 0.1', 'index', 'level_0'], axis = 1, inplace = True)
+dat.drop(['FID', 'field_1'], axis = 1, inplace = True)
+
+# replace "AVE" with "DEC"
+dat['mon'] = dat['mon'].str.replace('AVE', 'DEC')
+
+# concatenate mon and year
+removeExt = lambda x: str(x).split('.0')[0]
+dat['year'] = pd.DataFrame(list(map(removeExt, dat['year'])))
+dat['date'] = pd.DataFrame(pd.to_datetime(dat['year'].astype(str) + dat['mon'].astype(str), 
+                                             format = "%Y%b"))
+# concatenate row-columns-name
+dat['id'] = dat.row.astype(str).str.cat(dat['columns'].astype(str), sep = "_") + "_" + dat['name']
 
 
+datAll = dat[['id', 'name', 'date', 'mon', 'year', 'x_utm_m', 'y_utm_m', 'layer', 
+              'row', 'columns', 'county', 'use_class', 'withdrawal']]
+dat = dat[['id',  'date', 'x_utm_m', 'y_utm_m', 'layer', 'county', 'use_class', 'withdrawal']]
 
+# datAll.to_csv('ecftx_clipped_v4_extended.csv')
+# dat.to_csv('ecftx_clipped_v4_short.csv')
+
+print(dat)
 
 
 
 layers = dat['layer'].unique()
-monYear = dat['monYear'].unique()
+monYear = dat['date'].unique()
+
+print(layers)
+print(monYear)
 
 # loop through each monYear
 for my in monYear:
     os.chdir(dirHome)
     print(my)
-    currentDat = dat[dat['monYear'] == my]
+    
+    currentDat = dat[dat['date'] == my]
 
-    repWells = currentDat[currentDat['rowColPermit'].duplicated(keep = False)]
+    # fetch wells that traverse layers - all of them not skipping first     
+    repWells = currentDat[currentDat['id'].duplicated(keep = False)]
     # print(repWells)
 
     # drop repWells from original dataframe
@@ -54,8 +72,8 @@ for my in monYear:
 
     # save as csv
     os.chdir(dirOut)
-    repWells.to_csv(my.split('.')[0] + ".csv")
+    repWells.to_csv(str(my).split('-01T00:00:00.000000000')[0] + ".csv")
 
 # save truncated dat
 os.chdir(dirHome)
-dat.to_csv("ecftx_tr_20190329_GLRSTA_Counties_removedRepWells.csv")
+dat.to_csv("ecftx_clipped_v4_removedRepWells.csv")
